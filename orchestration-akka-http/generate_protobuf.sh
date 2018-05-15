07e85a4b3bbfa2a17d6b34010d83e39e058fd578
Marc Schlegel <marc.schlegel@gmx.de>
2018-05-15 21:08:31 +0200

    Protobuf generation added to Gradle build
    Protobuff-classes not commited any more

--- orchestration-akka-http/generate_protobuf.sh
+++ orchestration-akka-http/generate_protobuf.sh
@@ -1,3 +1,3 @@
 #scalapbc --scala_out=$(pwd)/src/main/scala --proto_path=$(pwd)/../protobuf/ bikes.proto
-scalapbc --scala_out=flat_package:src/main/scala-gen src/main/proto/bikes.proto
-scalapbc --scala_out=flat_package:src/main/scala-gen src/main/proto/wife.proto
+scalapbc --scala_out=flat_package:build/generated/source/proto/main/scala src/main/proto/bikes.proto
+scalapbc --scala_out=flat_package:build/generated/source/proto/main/scala src/main/proto/wife.proto
