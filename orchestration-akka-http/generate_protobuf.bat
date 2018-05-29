c96977efe95554774dbef420d5aa47f0d953c2aa
Marc Schlegel <marc.schlegel@sdv-it.de>
2018-05-29 11:26:14 +0200

    Generating Protobuf classes for Scala using Gradle for Windows and Linux

---
+++ orchestration-akka-http/generate_protobuf.bat
@@ -0,0 +1,2 @@
+call scalapbc -v3.5.1 --scala_out=flat_package:build/generated/source/proto/main/scala src/main/proto/bikes.proto
+call scalapbc -v3.5.1 --scala_out=flat_package:build/generated/source/proto/main/scala src/main/proto/wife.proto
--- orchestration-akka-http/generate_protobuf.sh
+++ orchestration-akka-http/generate_protobuf.sh
@@ -1,3 +1,2 @@
-#scalapbc --scala_out=$(pwd)/src/main/scala --proto_path=$(pwd)/../protobuf/ bikes.proto
-scalapbc --scala_out=flat_package:build/generated/source/proto/main/scala src/main/proto/bikes.proto
-scalapbc --scala_out=flat_package:build/generated/source/proto/main/scala src/main/proto/wife.proto
+scalapbc -v3.5.1 --scala_out=flat_package:build/generated/source/proto/main/scala src/main/proto/bikes.proto
+scalapbc -v3.5.1 --scala_out=flat_package:build/generated/source/proto/main/scala src/main/proto/wife.proto
