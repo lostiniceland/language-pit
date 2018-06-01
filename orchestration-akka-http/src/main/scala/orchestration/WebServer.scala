ce062466bbf0b022d59e94b3eca869c331aa50aa
Marc Schlegel <marc.schlegel@gmx.de>
2018-06-01 23:32:08 +0200

    Unit-Test Akka-Http routes
    Decouple route from Webserver as trait for easier testing.

--- orchestration-akka-http/src/main/scala/orchestration/WebServer.scala
+++ orchestration-akka-http/src/main/scala/orchestration/WebServer.scala
@@ -2,55 +2,60 @@ package orchestration
 
 import akka.actor.ActorRef
 import akka.http.scaladsl.model._
-import akka.http.scaladsl.server.{HttpApp, Route}
+import akka.http.scaladsl.server.{Directives, HttpApp, Route}
 import bikes.infrastructure.protobuf.BikeCreatedMessage
 import orchestration.Commands._
 import wife.infrastructure.protobuf.{BikeApprovedMessage, BikeRejectedMessage}
 
 
-object WebServer extends HttpApp with ProtobufSupport {
+object WebServer extends HttpApp with EventSubscriber {
   lazy val routerService: ActorRef = systemReference.get().actorOf(ServiceRouter.props(), "dispatcher")
 
-  override protected def routes: Route = pathPrefix("events") {
-    pathPrefix("bikes" / "created") {
-      get {
-        complete(StatusCodes.OK)
-      } ~
-        post {
-          decodeRequest {
-            entity(as[BikeCreatedMessage])(msg => {
-              routerService ! BikeCreated(msg.bikeId, msg.value)
-              complete(StatusCodes.Accepted)
-            })
-          }
-        }
-    } ~
-      pathPrefix("approvals") {
-        pathPrefix("accepted") {
-          post {
-            decodeRequest {
-              entity(as[BikeApprovedMessage])(msg => {
-                routerService ! BikeApproved(msg.bikeId)
-                complete(StatusCodes.Accepted)
-              })
-            }
-          }
-        } ~
-          pathPrefix("rejected") {
-            post {
-              decodeRequest {
-                entity(as[BikeRejectedMessage])(msg => {
-                  routerService ! BikeRejected(msg.bikeId)
-                  complete(StatusCodes.Accepted)
-                })
-              }
-            }
-          }
-      }
-  }
+  override protected def routes: Route = route
 
   override protected def postHttpBindingFailure(cause: Throwable): Unit = {
     systemReference.get().log.error("The server could not be started due to {}", cause)
     // TODO notify supervisor
+  }
+}
+
+
+trait EventSubscriber extends Directives with ProtobufSupport {
+
+  implicit val routerService: ActorRef
+
+  val route: Route = pathPrefix("events") {
+    pathPrefix("bikes" / "created") {
+      post {
+        decodeRequest {
+          entity(as[BikeCreatedMessage])(msg => {
+            routerService ! BikeCreated(msg.bikeId, msg.value)
+            complete(StatusCodes.Accepted)
+          })
+        }
+      }
+    } ~
+    pathPrefix("approvals") {
+      pathPrefix("accepted") {
+        post {
+          decodeRequest {
+            entity(as[BikeApprovedMessage])(msg => {
+              routerService ! BikeApproved(msg.bikeId)
+              complete(StatusCodes.Accepted)
+            })
+          }
+        }
+      } ~
+        pathPrefix("rejected") {
+          post {
+            decodeRequest {
+              entity(as[BikeRejectedMessage])(msg => {
+                routerService ! BikeRejected(msg.bikeId)
+                complete(StatusCodes.Accepted)
+              })
+            }
+          }
+        }
+    }
   }
 }
---
+++ orchestration-akka-http/src/test/scala/orchestration/EventSubscriberTest.scala
@@ -0,0 +1,41 @@
+package orchestration
+
+import akka.actor.ActorRef
+import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.testkit.ScalatestRouteTest
+import akka.testkit.TestProbe
+import bikes.infrastructure.protobuf.BikeCreatedMessage
+import org.scalatest.{Matchers, WordSpec}
+import wife.infrastructure.protobuf.{BikeApprovedMessage, BikeRejectedMessage}
+
+class EventSubscriberTest extends WordSpec with Matchers with ScalatestRouteTest with EventSubscriber {
+
+  val probe = TestProbe()
+
+  override implicit val routerService: ActorRef = probe.ref
+
+  "incoming requests" should {
+    // TODO atm Protobuff is able to unmarshall certain messages which are not typesaf (e.g. a BikeCreatedMessage can be unmarshalled into a BikeApprovedMessage)
+    "match POST 'events/bikes/created' with BikeCreatedMessage in Protobuf format" in {
+      val message = BikeCreatedMessage(bikeId = 1, value = 1).toByteArray
+      Post("/events/bikes/created").withEntity(ProtobufSupport.headerContentTypeProto, message) ~> route ~> check {
+        status shouldEqual StatusCodes.Accepted
+      }
+    }
+
+    "match POST 'events/approvals/accepted' for BikeApprovedMessage in Protobuf format" in {
+      val message = BikeApprovedMessage(bikeId = 1).toByteArray
+      Post("/events/approvals/accepted").withEntity(ProtobufSupport.headerContentTypeProto, message) ~> route ~> check {
+        status shouldEqual StatusCodes.Accepted
+      }
+    }
+
+    "match POST 'events/approvals/rejected' for BikeRejectedMessage in Protobuf format" in {
+      val message = BikeRejectedMessage(bikeId = 1).toByteArray
+      Post("/events/approvals/accepted").withEntity(ProtobufSupport.headerContentTypeProto, message) ~> route ~> check {
+        status shouldEqual StatusCodes.Accepted
+      }
+    }
+
+  }
+}
