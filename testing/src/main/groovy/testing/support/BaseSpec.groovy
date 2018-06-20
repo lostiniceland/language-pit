ce2af4de8b584176ad58416fbb6fb149ee2c34ec
Marc Schlegel <marc.schlegel@gmx.de>
2018-06-20 16:19:25 +0200

    Kafka connection and topic-configs must be passed as JVM-args

--- testing/src/main/groovy/testing/support/BaseSpec.groovy
+++ testing/src/main/groovy/testing/support/BaseSpec.groovy
@@ -9,9 +9,6 @@ abstract class BaseSpec extends Specification {
   KafkaEventConsumer consumer
 
   def setupSpec(){
-    System.properties.setProperty("KAFKA_HOST", "192.168.33.35")
-    System.properties.setProperty("KAFKA_PORT", "9092")
-    System.properties.setProperty("KAFKA_EVENT_TOPIC", "language-pit.events")
     consumer = new KafkaEventConsumer()
   }
 
