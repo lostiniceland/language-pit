81492be2e24632fc059e536510a2e036b8b19e42
Marc Schlegel <marc.schlegel@gmx.de>
2018-07-01 20:30:53 +0200

    Fixed broken test

--- orchestration-akka-http/src/test/scala/orchestration/EventSubscriberTest.scala
+++ orchestration-akka-http/src/test/scala/orchestration/EventSubscriberTest.scala
@@ -4,7 +4,7 @@ import akka.actor.ActorRef
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.testkit.ScalatestRouteTest
 import akka.testkit.TestProbe
-import common.infrastructure.protobuf._
+import common.infrastructure.protobuf.events._
 import org.scalatest.{Matchers, WordSpec}
 
 class EventSubscriberTest extends WordSpec with Matchers with ScalatestRouteTest with EventSubscriber {
