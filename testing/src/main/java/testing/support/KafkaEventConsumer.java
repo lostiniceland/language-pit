792b08b36d03bfbc51ab5a8da622bfef9f2c10a2
Marc Schlegel <marc.schlegel@gmx.de>
2018-07-03 18:28:58 +0200

    Event Consumer for tests now skips to the last available message within a topic
    Since the test only needs to listen to events after it has started the rebalancing is used to skip to the last entry for the "testing" group.

--- testing/src/main/java/testing/support/KafkaEventConsumer.java
+++ testing/src/main/java/testing/support/KafkaEventConsumer.java
@@ -2,15 +2,19 @@ package testing.support;
 
 import com.google.protobuf.InvalidProtocolBufferException;
 import common.infrastructure.protobuf.Events.EventsEnvelope;
-
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.Optional;
 import java.util.Properties;
 import java.util.function.Predicate;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRebalanceListener;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.serialization.ByteArrayDeserializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.slf4j.Logger;
@@ -25,7 +29,7 @@ public class KafkaEventConsumer {
   KafkaEventConsumer() {
     Properties defaultConfig = new Properties();
     try {
-      defaultConfig.load(getClass().getClassLoader().getResourceAsStream("testing.properties"));
+      defaultConfig.load(getClass().getResourceAsStream("/testing.properties"));
     } catch (IOException e) {
       logger.error("Could not load properties for testing", e);
     }
@@ -38,16 +42,34 @@ public class KafkaEventConsumer {
 
 
     Properties props = new Properties();
-    props.put("bootstrap.servers", kafkaHost + ":" + kafkaPort);
-    props.put("key.deserializer", StringDeserializer.class.getName());
-    props.put("value.deserializer", ByteArrayDeserializer.class.getName());
-    props.put("group.id", "testing");
+    props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaHost + ":" + kafkaPort);
+    props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+    props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ByteArrayDeserializer.class.getName());
+    props.put(ConsumerConfig.RETRY_BACKOFF_MS_CONFIG, 10000);
+    props.put(ConsumerConfig.GROUP_ID_CONFIG, "testing");
     this.kafkaConsumer = new KafkaConsumer<>(props);
-    kafkaConsumer.subscribe(Collections.singletonList(topic));
+
+    ConsumerRebalanceListener listener = new ConsumerRebalanceListener() {
+      @Override
+      public void onPartitionsRevoked(Collection<TopicPartition> partitions) {
+        logger.info("{} topic-partitions are revoked from this consumer", Arrays.toString(partitions.toArray()));
+      }
+
+      @Override
+      public void onPartitionsAssigned(Collection<TopicPartition> partitions) {
+        System.out.printf("%s topic-partitions are assigned to this consumer\n", Arrays.toString(partitions.toArray()));
+        for (TopicPartition topicPartition : partitions) {
+          System.out.println("Setting it to the end");
+          kafkaConsumer.seekToEnd(Collections.singletonList(topicPartition));
+        }
+      }
+    };
+
+    kafkaConsumer.subscribe(Collections.singletonList(topic), listener);
   }
 
   public Optional<EventsEnvelope> lookupEvent(Predicate<EventsEnvelope> predicate) {
-    ConsumerRecords<String, byte[]> records = kafkaConsumer.poll(500);
+    ConsumerRecords<String, byte[]> records = kafkaConsumer.poll(2000);
     try {
       for (ConsumerRecord<String, byte[]> record : records) {
         EventsEnvelope envelope = EventsEnvelope.parseFrom(record.value());
