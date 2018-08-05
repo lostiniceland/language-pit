9bc2ac3a4ea563a9a084c0f4a521b78e3374dfa9
Marc Schlegel <marc.schlegel@gmx.de>
2018-08-05 20:58:08 +0200

    KafkaConsumerActor receiving events

--- orchestration-akka-http/src/main/scala/orchestration/KafkaAcceptorActor.scala
+++ orchestration-akka-http/src/main/scala/orchestration/KafkaAcceptorActor.scala
@@ -2,11 +2,13 @@ package orchestration
 
 import akka.actor.{Actor, ActorLogging, ActorRef, Props}
 import cakesolutions.kafka.KafkaConsumer
+import cakesolutions.kafka.akka.KafkaConsumerActor.Subscribe.AutoPartition
 import cakesolutions.kafka.akka.KafkaConsumerActor.{Confirm, Subscribe, Unsubscribe}
 import cakesolutions.kafka.akka.{ConsumerRecords, KafkaConsumerActor}
 import com.typesafe.config.Config
 import common.infrastructure.protobuf.events.EventsEnvelope
 import common.infrastructure.protobuf.events.EventsEnvelope.Payload
+import common.infrastructure.protobuf.events.EventsEnvelope.Payload.BikeApprovalCreated
 import orchestration.Commands.{BikeApproved, BikeCreated, BikeDeleted, BikeRejected}
 import org.apache.kafka.common.serialization.{ByteArrayDeserializer, StringDeserializer}
 
@@ -27,7 +29,7 @@ class KafkaAcceptorActor (config: Config, routerService: ActorRef) extends Actor
         keyDeserializer = new StringDeserializer,
         valueDeserializer = new ByteArrayDeserializer
       ),
-      actorConf = KafkaConsumerActor.Conf(config.getConfig("actor")),
+      actorConf = KafkaConsumerActor.Conf(),
       downstreamActor = self
     ),
     "KafkaConsumer"
@@ -35,7 +37,7 @@ class KafkaAcceptorActor (config: Config, routerService: ActorRef) extends Actor
 
   override def preStart(): Unit = {
     super.preStart()
-    kafkaConsumerActor ! Subscribe
+    kafkaConsumerActor ! Subscribe.AutoPartition(List("language-pit.events")) // TODO subscribe not using config
   }
 
   override def postStop(): Unit = {
@@ -46,19 +48,18 @@ class KafkaAcceptorActor (config: Config, routerService: ActorRef) extends Actor
 
   override def receive: Receive = {
 
+//    case KafkaAcceptorActor.extractor(records) =>
+
     // TODO find a way to use an extractor as mentioned in docu
     case consumerRecords: ConsumerRecords[String, Array[Byte]] =>
-      val x: Seq[EventsEnvelope] = consumerRecords.values.map(value => EventsEnvelope.parseFrom(value))
-      for (envelope <- x) {
+      consumerRecords.values.map(value => EventsEnvelope.parseFrom(value)).foreach(envelope =>
         envelope.payload match {
-          case approvalEvent: Payload.BikeApprovalCreated => ???
+          case approvalEvent: Payload.BikeApprovalCreated => routerService ! BikeApprovalCreated(approvalEvent.value)
           case bikeEvent: Payload.BikeApproved => routerService ! BikeApproved(bikeEvent.value.bikeId)
           case bikeEvent: Payload.BikeCreated => routerService ! BikeCreated(bikeEvent.value.bikeId, bikeEvent.value.value)
           case bikeEvent: Payload.BikeDeleted => routerService ! BikeDeleted(bikeEvent.value.bikeId)
           case bikeEvent: Payload.BikeRejected => routerService ! BikeRejected(bikeEvent.value.bikeId)
-        }
-      }
-
+        })
       // By committing *after* processing we get at-least-once-processing, but that's OK here because we can identify duplicates by their timestamps
       kafkaConsumerActor ! Confirm(consumerRecords.offsets, commit = true)
 
