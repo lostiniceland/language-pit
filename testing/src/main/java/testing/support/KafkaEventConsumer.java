4dfa999a78593b54c768c1dbc94e63834761038a
Marc Schlegel <marc.schlegel@gmx.de>
2018-08-09 23:41:26 +0200

    Do not care about older events.
    This is not necessary when running in integration-tests because the kafka-containers are recreated, but it might be easier when testing against a existing cluster.

--- testing/src/main/java/testing/support/KafkaEventConsumer.java
+++ testing/src/main/java/testing/support/KafkaEventConsumer.java
@@ -106,6 +106,7 @@ public class KafkaEventConsumer {
       props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
       props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ByteArrayDeserializer.class.getName());
       props.put(ConsumerConfig.GROUP_ID_CONFIG, "testing");
+      props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
       this.kafkaConsumer = new KafkaConsumer<>(props);
 
       // Use manual partition-assignment to not fall into rebalancing-issues
