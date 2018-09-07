1a1fcb43c44229bbf9b8398d404b94697531be4f
Marc Schlegel <marc.schlegel@gmx.de>
2018-09-07 21:55:54 +0200

    Support Multiple Consumers
    - added consumer strategy to roundrobin
    - added additional partion to some compose files for testing
    
    Now both orchestration approaches can be used together (not saying it makes sense).
    In case of Akka this means the application can scale to multiple instances

--- orchestration-camunda/src/main/java/orchestration/camunda/KafkaConsumerTask.java
+++ orchestration-camunda/src/main/java/orchestration/camunda/KafkaConsumerTask.java
@@ -16,6 +16,7 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.consumer.RoundRobinAssignor;
 import org.apache.kafka.common.errors.WakeupException;
 import org.apache.kafka.common.serialization.ByteArrayDeserializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
@@ -48,7 +49,7 @@ public class KafkaConsumerTask implements Runnable, ManagedTask {
     props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
     props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ByteArrayDeserializer.class.getName());
     props.put(ConsumerConfig.GROUP_ID_CONFIG, "orchestration");
-    props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
+    props.put(ConsumerConfig.PARTITION_ASSIGNMENT_STRATEGY_CONFIG, "org.apache.kafka.clients.consumer.RoundRobinAssignor");
     props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest"); // TODO use confirmed offset
 
     this.kafkaConsumer = new KafkaConsumer<>(props);
