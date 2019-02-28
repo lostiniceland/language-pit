3a9aa123080a04709a95d5ed58eb85924d6d8cec
Marc Schlegel <marc.schlegel@gmx.de>
2019-02-28 14:32:24 +0100

    Switch to Postgres
    Liberty can also be generated via Gradle

---
+++ docker-compose/postgres-init/init.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+  revoke all on database postgres from public;
+	create user bikes with password '1234';
+	create user wife with password '4321';
+	// grant connect on database postgres to bikes;
+  // grant connect on database postgres to wife;
+grant all on database postgres to bikes;
+grant all on database postgres to wife;
+	create schema authorization bikes;
+	create schema authorization wife;
+	alter user bikes set search_path to 'bikes';
+	alter user wife set search_path to 'wife';
+EOSQL
--- javaee/build.gradle.kts
+++ javaee/build.gradle.kts
@@ -1,14 +1,25 @@
 import com.bmuschko.gradle.docker.tasks.image.DockerBuildImage
 import com.bmuschko.gradle.docker.tasks.image.Dockerfile
+import com.github.kittinunf.fuel.Fuel
 import com.google.protobuf.gradle.protobuf
 import com.google.protobuf.gradle.protoc
+import org.gradle.kotlin.dsl.support.unzipTo
+import java.nio.file.Files
+import java.nio.file.Paths
+
+buildscript {
+	dependencies {
+		classpath(group = "com.github.kittinunf.fuel", name = "fuel", version = "2.0.1")
+		classpath(group = "com.github.kittinunf.result", name = "result", version = "2.1.0")
+	}
+}
 
 plugins {
 	java
 	war
 	groovy
 	id("com.google.protobuf") version "0.8.8" 
-	id("com.bmuschko.docker-remote-api") version "4.4.1" 
+	id("com.bmuschko.docker-remote-api") version "4.4.1"
 }
 
 version = "0.1.0"
@@ -30,9 +41,10 @@ dependencies {
 	compile(group = "com.google.protobuf", name = "protobuf-java", version = versionProtobuf)
 	compile(group = "com.googlecode.protobuf-java-format", name = "protobuf-java-format", version = "1.4")
 	compileOnly(group = "com.fasterxml.jackson.core", name = "jackson-databind", version = "2.9.5")
-	compile(group = "org.slf4j", name = "slf4j-api", version = "1.+")
-	compile(group = "ch.qos.logback", name = "logback-classic", version = "1.2.3")
+	compile(group = "org.slf4j", name = "slf4j-api", version = "1.7.26")
+	compile(group = "org.slf4j", name = "slf4j-simple", version = "1.7.26")
 	compile(group = "org.apache.kafka", name = "kafka-clients", version = "2.0.0")
+	compile(group = "org.flywaydb", name = "flyway-core", version = "6.0.0-beta") // beta due to Postgres 11
 
 	testCompile(group = "junit", name = "junit", version = "4.+")
 	testCompile(group = "org.codehaus.groovy", name = "groovy-all", version = "2.4.+")
@@ -71,22 +83,67 @@ protobuf {
 
 
 val libertyDockerFolder = "$buildDir/docker/openliberty"
+val libertyConfigFolder = "${project.projectDir}/config-resources-openliberty"
+val libertyInstallFolder = "${project.projectDir}/server"
+val libertyServerFolder = "$libertyInstallFolder/wlp/usr/servers/default"
+val libertyServerPostgresFolder = "$libertyServerFolder/lib-postgres"
 
 tasks {
+
+	register("createServer"){
+		group = "server setup"
+		description = "Downloads and extracts a OpenLiberty server instance and configures the Postgres jdbc-driver"
+		doLast{
+			val libertyDownloadZip = File(project.buildDir.absolutePath + "/wlp.zip")
+
+			if(!libertyDownloadZip.exists()) {
+				println("Download Openliberty from Maven-Central")
+				Fuel.download("http://central.maven.org/maven2/io/openliberty/openliberty-javaee8/19.0.0.1/openliberty-javaee8-19.0.0.1.zip")
+						.fileDestination { _, _ -> libertyDownloadZip }
+//						.progress {readBytes, totalBytes ->  println("${readBytes.toFloat() / totalBytes.toFloat() * 100} %")}
+						.response()
+			}
+			unzipTo(File(libertyInstallFolder), libertyDownloadZip)
+
+			val postgresDownloadJar = File("$libertyServerPostgresFolder/postgresql-42.2.5.jar")
+
+			if(!postgresDownloadJar.exists()) {
+				Files.createDirectories(Paths.get(libertyServerPostgresFolder))
+				println("Download Postgres driver from Maven-Central")
+				Fuel.download("http://central.maven.org/maven2/org/postgresql/postgresql/42.2.5/postgresql-42.2.5.jar")
+						.fileDestination { _, _ -> postgresDownloadJar }
+//						.progress {readBytes, totalBytes ->  println("${readBytes.toFloat() / totalBytes.toFloat() * 100} %")}
+						.response()
+			}
+		}
+	}
+
+	register("updateServer"){
+		group = "server setup"
+		description = "Copies the configureation-resource to the server directory"
+
+		doLast {
+			copy {
+				from(libertyConfigFolder)
+				into(libertyServerFolder)
+			}
+		}
+	}
+
 	register("prepareDocker") {
 		dependsOn(war)
 		doLast {
 			copy {
 				from(war.get().archiveFile)
-				from(project.projectDir.toString() + "/server")
+				from(libertyConfigFolder)
 				into(libertyDockerFolder)
 				rename { fileName ->
 					fileName.replaceFirst(war.get().archiveFileName.get(), "bikes.war", false)
 				}
 			}
 			copy {
-				from(project.projectDir.absolutePath + "/derbyLib/")
-				into("$libertyDockerFolder/derbyLib")
+				from(libertyServerPostgresFolder)
+				into("$libertyDockerFolder/lib-postgres")
 			}
 		}
 	}
